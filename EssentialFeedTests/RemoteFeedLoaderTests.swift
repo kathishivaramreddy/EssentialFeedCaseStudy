54b4c565ce65e90ce0a0fc6f2496ccb1d2391a56
kathishivaramreddy <kathishivaram@gmail.com>
2021-01-21 01:03:24 +0530

    Refactor remove code duplication in tests

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -44,23 +44,29 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
         
-        let client = HTTPClientSpy()
-        let url = URL(string: "https://a-example.com")!
-        
-        let _ = RemoteFeedLoader(url: url, client: client)
+        let (_, client) = makeSUT()
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestsDataFromURL() {
 
-        let client = HTTPClientSpy()
         let url = URL(string: "https://a-example.com")!
-        let sut = RemoteFeedLoader(url: url, client: client)
-
-
+        let (sut, client) = makeSUT(url: url)
+        
         sut.load()
 
         XCTAssertNotNil(client.requestedURL, "https://a-example.com")
+    }
+    
+    //Mark:- Helper
+    
+    func makeSUT(url: URL = URL(string: "https://a-example.com")!) -> (sut: RemoteFeedLoader, cleint: HTTPClientSpy) {
+        
+        let client = HTTPClientSpy()
+        
+        let sut = RemoteFeedLoader(url: url, client: client)
+        
+        return (sut,client)
     }
 }
