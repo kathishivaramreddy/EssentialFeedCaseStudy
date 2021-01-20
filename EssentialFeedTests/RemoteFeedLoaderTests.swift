6b6e28af07d7a20dd38da1dce711a56a0f8649b6
kathishivaramreddy <kathishivaram@gmail.com>
2021-01-21 00:58:08 +0530

    Passing client and url to RemoteFeedLoader

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -10,21 +10,57 @@ import XCTest
 
 class RemoteFeedLoader {
     
+    private let client: HTTPClient
+    private let url: URL
     
+    init(url: URL, client: HTTPClient) {
+        
+        self.url = url
+        self.client = client
+    }
+    
+    func load() {
+        
+        client.get(from: self.url)
+    }
 }
 
-class HTTPClient {
+protocol HTTPClient {
+    
+    func get(from url: URL)
+}
+
+class HTTPClientSpy: HTTPClient {
     
     var requestedURL: URL?
+
+    func get(from url: URL) {
+        
+        self.requestedURL = url
+    }
 }
 
 class RemoteFeedLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
         
-        let _ = RemoteFeedLoader()
-        let client = HTTPClient()
+        let client = HTTPClientSpy()
+        let url = URL(string: "https://a-example.com")!
+        
+        let _ = RemoteFeedLoader(url: url, client: client)
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestsDataFromURL() {
+
+        let client = HTTPClientSpy()
+        let url = URL(string: "https://a-example.com")!
+        let sut = RemoteFeedLoader(url: url, client: client)
+
+
+        sut.load()
+
+        XCTAssertNotNil(client.requestedURL, "https://a-example.com")
     }
 }
