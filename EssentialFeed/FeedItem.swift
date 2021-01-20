44452fc473d6437699b18d30a840f2a57b69dd0b
kathishivaramreddy <kathishivaram@gmail.com>
2021-01-21 00:13:04 +0530

    Create FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by ShivaRamReddy on 21/01/21.
+//
+
+
+struct FeedItem {
+    
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
