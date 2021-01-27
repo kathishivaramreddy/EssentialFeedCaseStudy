cd701472869cff4e33a92d0beb2a9535820bf0f8
kathishivaramreddy <kathishivaram@gmail.com>
2021-01-28 01:36:28 +0530

    Delete cache when save is called from local feed loader

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -8,17 +8,30 @@
 import Foundation
 import XCTest
 
+import EssentialFeed
+
 class FeedStore {
     
-    let deleteCacheCount = 0
+    var deleteCacheFeedCallCount = 0
+    
+    func deleteCacheFeed() {
+        
+        deleteCacheFeedCallCount += 1
+    }
 }
 
 class LocalFeedLoader {
     
     let store: FeedStore
+    
     init(store: FeedStore) {
         
         self.store = store
+    }
+    
+    func save(items: [FeedItem]) {
+        
+        store.deleteCacheFeed()
     }
 }
 
@@ -30,6 +43,28 @@ class CacheFeedUseCaseTests: XCTestCase {
         
         let _ = LocalFeedLoader(store: store)
         
-        XCTAssertEqual(store.deleteCacheCount, 0)
+        XCTAssertEqual(store.deleteCacheFeedCallCount, 0)
+    }
+    
+    func test_save_requestsCacheDeletion() {
+        
+        let store = FeedStore()
+        
+        let sut = LocalFeedLoader(store: store)
+        
+        let items = [uniqueItem()]
+        sut.save(items: items)
+        
+        XCTAssertEqual(store.deleteCacheFeedCallCount, 1)
+    }
+    
+    //Marker: Helpers
+    
+    private func uniqueItem() -> FeedItem {
+        return FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
+    }
+    
+    private func anyURL() -> URL {
+        return URL(string: "http://any-url.com")!
     }
 }
