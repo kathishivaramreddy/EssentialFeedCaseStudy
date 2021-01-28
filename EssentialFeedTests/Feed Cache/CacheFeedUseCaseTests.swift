63175d8afeb93ff7aee23d43862b3cb05243b5a8
kathishivaramreddy <kathishivaram@gmail.com>
2021-01-28 23:03:14 +0530

    Move feedstore spy to its own file

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -10,52 +10,6 @@ import XCTest
 
 import EssentialFeed
 
-class FeedStoreSpy: FeedStore {
-            
-    private var deletionCompletions = [DeletionCompletion]()
-    private var insertionCompletion = [InsertionCompletion]()
-    
-    enum ReceivedMessage: Equatable {
-        
-        case deletion
-        case insertion(items: [LocalFeedImage], date: Date)
-    }
-    
-    var receivedMessage = [ReceivedMessage]()
-    
-    func deleteCacheFeed(completion: @escaping DeletionCompletion) {
-        
-        deletionCompletions.append(completion)
-        receivedMessage.append(.deletion)
-    }
-    
-    func completeDeletion(with error: Error, at index: Int = 0) {
-        
-        deletionCompletions[index](error)
-    }
-    
-    func completeDeletionSuccessfully(at index: Int = 0) {
-        
-        deletionCompletions[index](nil)
-    }
-    
-    func completeInsertion(with error: Error, at index: Int = 0) {
-        
-        insertionCompletion[index](error)
-    }
-    
-    func insert(items: [LocalFeedImage], currentDate: Date, completion: @escaping InsertionCompletion) {
-        
-        insertionCompletion.append(completion)
-        receivedMessage.append(.insertion(items: items, date: currentDate))
-    }
-    
-    func completeInsertionSuccessfully(at index: Int = 0) {
-        
-        insertionCompletion[index](nil)
-    }
-}
-
 class CacheFeedUseCaseTests: XCTestCase {
     
     func test_init_doesNotDeleteCacheUponCreate() {
---
+++ EssentialFeedTests/Helper/FeedStoreSpy.swift
@@ -0,0 +1,55 @@
+//
+//  FeedStoreSpy.swift
+//  EssentialFeedTests
+//
+//  Created by ShivaRamReddy on 28/01/21.
+//
+
+import Foundation
+import EssentialFeed
+
+class FeedStoreSpy: FeedStore {
+            
+    private var deletionCompletions = [DeletionCompletion]()
+    private var insertionCompletion = [InsertionCompletion]()
+    
+    enum ReceivedMessage: Equatable {
+        
+        case deletion
+        case insertion(items: [LocalFeedImage], date: Date)
+    }
+    
+    var receivedMessage = [ReceivedMessage]()
+    
+    func deleteCacheFeed(completion: @escaping DeletionCompletion) {
+        
+        deletionCompletions.append(completion)
+        receivedMessage.append(.deletion)
+    }
+    
+    func completeDeletion(with error: Error, at index: Int = 0) {
+        
+        deletionCompletions[index](error)
+    }
+    
+    func completeDeletionSuccessfully(at index: Int = 0) {
+        
+        deletionCompletions[index](nil)
+    }
+    
+    func completeInsertion(with error: Error, at index: Int = 0) {
+        
+        insertionCompletion[index](error)
+    }
+    
+    func insert(items: [LocalFeedImage], currentDate: Date, completion: @escaping InsertionCompletion) {
+        
+        insertionCompletion.append(completion)
+        receivedMessage.append(.insertion(items: items, date: currentDate))
+    }
+    
+    func completeInsertionSuccessfully(at index: Int = 0) {
+        
+        insertionCompletion[index](nil)
+    }
+}
