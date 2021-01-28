e908adec8e1263c4a7c4c00fe97d58c52ccaab4a
kathishivaramreddy <kathishivaram@gmail.com>
2021-01-28 22:57:14 +0530

    Localfeedloader does not receive message upon creation

---
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -0,0 +1,56 @@
+//
+//  LoadFeedFromCacheUseCaseTests.swift
+//  EssentialFeedTests
+//
+//  Created by ShivaRamReddy on 28/01/21.
+//
+
+import Foundation
+import XCTest
+
+import EssentialFeed
+
+class LoadFeedFromCacheUseCaseTests: XCTestCase {
+    
+    func test_init_doesNotRecieveMessage() {
+        
+        let (_, store) = makeSUT()
+        
+        XCTAssertEqual(store.receivedMessage, [])
+    }
+    
+    //Marker: Helpers
+    
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
+        
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: store, items: uniqueItems().models, currentDate: currentDate)
+        
+        trackMemoryLeak(sut, file: file, line: line)
+        trackMemoryLeak(store, file: file, line: line)
+        
+        return (sut, store)
+    }
+    
+    private func uniqueItem() -> FeedImage {
+        return FeedImage(id: UUID(), description: "any", location: "any", imageURL: anyURL())
+    }
+    
+    private func uniqueItems() -> (models: [FeedImage], localItems: [LocalFeedImage]) {
+        
+        let items = [uniqueItem(), uniqueItem()]
+        let localItems = items.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, imageURL: $0.imageURL) }
+        
+        return (items, localItems)
+    }
+    
+    private func anyURL() -> URL {
+        return URL(string: "http://any-url.com")!
+    }
+    
+    private func anyNSError() -> NSError {
+        
+        NSError(domain: "any error", code: 0, userInfo: nil)
+    }
+
+}
