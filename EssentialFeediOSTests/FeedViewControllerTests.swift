caa503209eb86abe99b13b4d72ebc7a1d8443044
kathishivaramreddy <kathishivaram@gmail.com>
2021-03-03 20:15:37 +0530

    Change access modifier of loadedCellCount to private set

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import XCTest
+
+class FeedViewController {
+    
+    init(loader: FeedViewControllerTests.LoaderSpy) {
+        
+    }
+}
+
+class FeedViewControllerTests: XCTestCase {
+ 
+    func test_init_doesNotLoadFeed() {
+        
+        let loader = LoaderSpy()
+        let _ = FeedViewController(loader: loader)
+        
+        XCTAssertEqual(loader.loadedCellCount, 0)
+    }
+    
+    class LoaderSpy {
+        
+        private(set) var loadedCellCount = 0
+    }
+}
